0d6f00ba6fed929d15587ee66e53537863aadd6d
lzanabria <lazanabria2@misena.edu.co>
2022-10-02 22:52:54 -0500

    Parámetros de funciones

--- README.md
+++ README.md
@@ -75,3 +75,15 @@ let dia=(num_dia<=dias.length) ? dias[num_dia-1] : "¡N° de día seleccionado n
 alert("\t Día seleccionado: "+dia);
 ```
 
+#### **Parámetros de funciones.**
+
+Los parámetros predeterminados de función permiten que los parámetros con nombre se inicien con valores predeterminados si no se pasa ningún valor.
+
+```js
+function multiplicar(a, b=10) {
+    return a*b;
+}
+alert("▶ a=3 \n▶ b=9\n\t Resultado: "+multiplicar(3, 9)); //Se le ha dado un valor a=2 y b=9
+alert("▶ a=5 \n▶ b=No se le asignó un valor\n\t Resultado: "+multiplicar(5)); //Sólo se le dió valor a=5 y a la b no se le dió ningún valor, de manera que toma su valor predeterminado que es b=10
+```
+
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let num_dia=Number(prompt("\t Ingrese el número del día que desea seleccionar: ", 5));
-        let dias=["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
-        let dia=(num_dia<=dias.length) ? dias[num_dia-1] : "¡N° de día seleccionado no existe!";
-        alert("\t Día seleccionado: "+dia);
+        function multiplicar(a, b=10) {
+            return a*b;
+        }
+        alert("▶ a=3 \n▶ b=9\n\t Resultado: "+multiplicar(3, 9)); //Se le ha dado un valor a=2 y b=9
+        alert("▶ a=5 \n▶ b=No se le asignó un valor\n\t Resultado: "+multiplicar(5)); //Sólo se le dió valor a=5 y a la b no se le dió ningún valor, de manera que toma su valor predeterminado que es b=10
     }, 500);
 });
